998d243094a46b458a7dc08e1de3b54239e8aaac
Dinesh <dkumar.270985@gmail.com>
2020-05-16 20:50:00 +0530

    bubble progation added

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,11 +1,22 @@
 import {Component} from "@angular/core";
 @Component({
   selector: 'courses',
-  template: `<button  class="btn btn-primary" [class.active]="isClicked"> Save</button>`
+  template: `
+  <div (click)="onDivClicked()">
+    <button  (click)="onClicked($event)"> Save</button>
+    </div>`
 
 })
 export class CoursesComponent{
   title = "Welcome";
   isClicked=true;
+  onClicked($event) {
+    $event.stopPropagation();
+    console.log("Button Clicked",$event);
+  }
+  onDivClicked(){
+
+    console.log("Div Clicked");
+  }
 
 }
